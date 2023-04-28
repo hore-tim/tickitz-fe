7ccceadc8af79715fab0213ccda691ceb49c129a
wyakaga <123617005+wyakaga@users.noreply.github.com>
2023-04-28 20:13:36 +0700

    add
    add:
    * setup tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				tickitz: {
+					primary: "3D405B",
+					secondary: "F4F1DE",
+					warning: "F2CC8F",
+					error: "E07A5F",
+					success: "81B29A",
+					darkTitle: "#14142B",
+					label: "#6E7191",
+					basic: "#4E4B66",
+				},
+			},
+		},
+	},
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [],
+	},
+};
