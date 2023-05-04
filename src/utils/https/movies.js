3111ec9221f116f233469a8a26ff3439aa791162
digi-squad <digitalsquadfw14@proton.me>
2023-05-04 18:36:47 +0700

    done

--- src/utils/https/movies.js
+++ src/utils/https/movies.js
@@ -8,15 +8,9 @@ export const getAllMovies = (limit, sort, page, search, controller) => {
 	return axios.get(url, config);
 };
 
-<<<<<<< HEAD
-export const getMovies = (controller) => {
-	const url = `${baseUrl}/movies?limit=5`;
-	const config = {signal: controller.signal}
-=======
 export const getSingleMovie = (id, controller) => {
 	const url = `${baseUrl}/movies/${id}`
 	const config = { signal: controller.signal };
->>>>>>> 954be6b54b0701ebc67409707dec8ba08b1266d2
 	return axios.get(url, config);
 }
 
